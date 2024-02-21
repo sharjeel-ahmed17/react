a3eec6b2f9eda57752016114d2bda3a949beb03c
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-02-21 21:20:03 +0500

    more

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import webImg from "./images/react-devtools-standalone.png"
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
+import { blogs } from './data/blogs';
 function App() {
 
   const addData = () => {
@@ -22,7 +23,13 @@ function App() {
       <button onClick={addData} className='bg-danger text-success p-4' > save</button>
       <button onClick={() => addDataInTable(10, 20)} className='bg-danger text-success p-4' >add data</button>
       <Header title="hello world" description="welcome to sharjeel ahmed company" />
-      <productItem />
+      {blogs.map((v, i) => {
+        return (
+
+          <ProductItem pitems={v} key={i} />
+        )
+
+      })}
       <Footer />
 
 
@@ -35,21 +42,11 @@ export default App;
 
 
 
-function productItem() {
+function ProductItem({pitems}) {
+  console.log(pitems.title)
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-3 col-sm-6">
-          <div className="card" style={{ width: "18rem" }} >
-
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text" style={{ color: "red", background: "blue" }}>sharjeel ali bari</p>
-              <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
+     
     </div>
   )
 }
